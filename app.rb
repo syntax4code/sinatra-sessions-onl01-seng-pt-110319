144201063316821491c58056b2e36cddeeef443c
Warren Harper II <warrenharper2@att.net>
2020-02-21 23:37:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -37,6 +37,7 @@ class App < Sinatra::Base
 
   get '/set_session' do
     #set session id here
+    set session[:id] == 1
 
     if session[:id] == 1
       # "Session ID set. It's currently set to #{session[:id]}."
